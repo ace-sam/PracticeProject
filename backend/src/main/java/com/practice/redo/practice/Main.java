bae860e5fe3e9f583f36bf401df3eb4dca0686b8
ace-sam <ace0sam@yahoo.com>
2024-03-06 17:56:41 +0100

    CD Workflow docker file path updated

--- backend/src/main/java/com/practice/redo/practice/Main.java
+++ backend/src/main/java/com/practice/redo/practice/Main.java
@@ -8,7 +8,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import java.util.List;
 import java.util.Random;
 
 @SpringBootApplication
@@ -17,8 +16,6 @@ public class Main {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
-
-
 	}
 
 
